cb55397bb7a0669b82af18f5a7c41a1c3a287ab2
bkrishna1114 <bkrishna1114@gmail.com>
2023-06-15 01:55:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+//this is a commnet section
+console.log("hello world");
+
+
+//variable...
+//cannot be a reserved keyword
+// should be a meanignful eg.desctiptive names..
+// cannot start with number eg 1name
+// Cannot contain space or hyphen
+// Are case sensitive..
+
+// var a = 20;
+console.log(20);
+
+let b; //undefined
+console.log(b);
+
+let name = "Bala";
+console.log(name);
+
+//camerl notaion..
+let firstName;
+let LastName;
+
+firstName = "Bala Krishna";
+LastName = 'baddi'
+
+console.log(firstName," ",LastName)
+
+
+//constants..
+let salary = 4000;
+console.log(salary);
+salary =50;
+console.log(salary);
+
+const salary2 = 4000;
+console.log(salary2);
+// salary2 =50;  This will throgn an error...
+console.log('Cannot assigne value to the constant...')
+
+
+//premitive/values Types (String,Number,Boolen,Undefined,null)
+let a = 'bala'; //string literal..
+let number = 30; //number literal
+let isdone = false; //boolean literal
+let un = undefined; //undefined
+let selected = null; // null
+
+console.log('-------------------->',a,number,isdone,un,selected)
+
+
