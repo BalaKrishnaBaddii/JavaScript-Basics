5fdf6a1a4729d4c281c7c8c2693d197a4149de07
bkrishna1114 <bkrishna1114@gmail.com>
2024-05-29 21:35:44 +0530

    Recuce function

--- Reduce/reduce.js
+++ Reduce/reduce.js
@@ -11,6 +11,21 @@ const AllPages = data.reduce((nopages, book) => nopages + book.pages, 0);
 console.log(AllPages);
 
 let x = [1, 2, 3, 4, 5];
-const additon = x.reduce((n, x) => n + x, 0);
+const additon = x.reduce((n, x) => n * x, 1);
 
 additon;
+
+const getFraction = (n) => {
+  if (n === 0 || n === 1) {
+    return true;
+  }
+
+  //   const numbers = Array.from({ length: n }, (_, i) => i + 1);
+
+  return Array.from({ length: n }, (_, i) => i + 1).reduce(
+    (num, currentvlue) => num * currentvlue,
+    1
+  ); // current value is keep iterate itself
+};
+
+console.log(getFraction(5));
